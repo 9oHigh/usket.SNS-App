4b9f59a7e2ce1a5fe6b682f5515e0953206b9b31
jinyuri <skec040401@naver.com>
2024-11-13 16:17:15 +0900

    Refactor: 프로필 스크린 코드 수정 및 분리

--- lib/presentation/screens/profile/provider/state/profile_state.dart
+++ lib/presentation/screens/profile/provider/state/profile_state.dart
@@ -1,3 +1,5 @@
+import 'package:sns_app/data/models/user.dart';
+
 class ProfileState {
   final User? user;
   final bool isLoading;
@@ -18,51 +20,6 @@ class ProfileState {
       user: user ?? this.user,
       isLoading: isLoading ?? this.isLoading,
       error: error ?? this.error,
-    );
-  }
-}
-
-// user_model.dart
-class User {
-  final String id;
-  final String username;
-  final String profileImageUrl;
-  final String bio;
-  final int followers;
-  final int following;
-  final int posts;
-  final String email;
-
-  User({
-    required this.id,
-    required this.username,
-    required this.profileImageUrl,
-    required this.bio,
-    required this.followers,
-    required this.following,
-    required this.posts,
-    required this.email,
-  });
-
-  User copyWith({
-    String? id,
-    String? username,
-    String? profileImageUrl,
-    String? bio,
-    int? followers,
-    int? following,
-    int? posts,
-    String? email,
-  }) {
-    return User(
-      id: id ?? this.id,
-      username: username ?? this.username,
-      profileImageUrl: profileImageUrl ?? this.profileImageUrl,
-      bio: bio ?? this.bio,
-      followers: followers ?? this.followers,
-      following: following ?? this.following,
-      posts: posts ?? this.posts,
-      email: email ?? this.email,
     );
   }
 }
