e993874a46b9cd6f90f46d5759a1bf733019733e
[APP2]이경후 <usket@icloud.com>
2024-11-18 13:30:15 +0000

    fix: Signin -> signin

--- lib/presentation/screens/signin/provider/signin_notifier_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sns_app/presentation/screens/signin/provider/state/Signin_notifier.dart';
-import 'package:sns_app/presentation/screens/signin/provider/state/Signin_state.dart';
-
-final signinNotifierProvider =
-    AutoDisposeStateNotifierProvider<SigninNotifier, SigninState>(
-  (ref) => SigninNotifier(),
-);
